5b58d904959efb147c1b3134bc697375f9018230
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-02-14 08:57:27 +0100

    Change ClientFactory into interface

--- src/main/java/pl/mrugames/commons/host/ClientFactory.java
+++ src/main/java/pl/mrugames/commons/host/ClientFactory.java
@@ -2,8 +2,6 @@ package pl.mrugames.commons.host;
 
 import java.net.Socket;
 
-public class ClientFactory {
-    void create(Socket socket) {
-
-    }
+public interface ClientFactory {
+    void create(Socket socket);
 }
