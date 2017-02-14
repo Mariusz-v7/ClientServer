d41b2b6e77ce45e1871572c8887ad14172dc42a4
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-02-14 09:10:14 +0100

    Move Client Factory to separate package

--- src/main/java/pl/mrugames/commons/host/Host.java
+++ src/main/java/pl/mrugames/commons/host/Host.java
@@ -2,6 +2,7 @@ package pl.mrugames.commons.host;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.mrugames.commons.client.ClientFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
