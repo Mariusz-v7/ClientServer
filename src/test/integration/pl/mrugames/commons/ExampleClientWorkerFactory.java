759637fc0cff63ed1364e19d0047383f9335ceef
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-02-18 14:01:16 +0100

    Refactor examples

---
+++ src/test/integration/pl/mrugames/commons/ExampleClientWorkerFactory.java
@@ -0,0 +1,18 @@
+package pl.mrugames.commons;
+
+import pl.mrugames.commons.client.ClientWorker;
+import pl.mrugames.commons.client.ClientWorkerFactory;
+import pl.mrugames.commons.client.Comm;
+
+public class ExampleClientWorkerFactory implements ClientWorkerFactory<String, String> {
+    private final Runnable onShutdownCommand;
+
+    public ExampleClientWorkerFactory(Runnable onShutdownCommand) {
+        this.onShutdownCommand = onShutdownCommand;
+    }
+
+    @Override
+    public ClientWorker create(String name, Comm<String, String> comm, Runnable shutdownSwitch) {
+        return new ExampleClientWorker(name, comm, shutdownSwitch, onShutdownCommand);
+    }
+}
