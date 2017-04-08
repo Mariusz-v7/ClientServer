9cd6e2115dc3844b3b5f6e23d0b3241a972d3a2e
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-04-08 09:03:26 +0200

    Add Client Factory Builder

--- src/test/integration/pl/mrugames/commons/client_example/Main.java
+++ src/test/integration/pl/mrugames/commons/client_example/Main.java
@@ -3,12 +3,12 @@ package pl.mrugames.commons.client_example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.mrugames.commons.client.ClientFactory;
+import pl.mrugames.commons.client.helpers.ClientFactoryBuilder;
 import pl.mrugames.commons.client.io.TextReader;
 import pl.mrugames.commons.client.io.TextWriter;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
@@ -25,16 +25,10 @@ public class Main {
         final String address = args[0];
         final int port = Integer.valueOf(args[1]);
 
-        ClientFactory clientFactory = new ClientFactory<>(
-                "Local Client",
-                60,
-                TextWriter::new,
-                TextReader::new,
-                new LocalClientWorkerFactory(),
-                Collections.emptyList(),
-                Collections.emptyList(),
-                Collections.emptyList()
-        );
+        ClientFactory<String, String, String, String> clientFactory =
+                new ClientFactoryBuilder<>(TextWriter::new, TextReader::new, new LocalClientWorkerFactory())
+                        .setClientName("Local Client")
+                        .build();
 
         LocalClientWorker localClientWorker = (LocalClientWorker) clientFactory.create(new Socket(address, port)).get();
 
--- src/test/integration/pl/mrugames/commons/telnet_example/Main.java
+++ src/test/integration/pl/mrugames/commons/telnet_example/Main.java
@@ -3,17 +3,16 @@ package pl.mrugames.commons.telnet_example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.mrugames.commons.client.ClientFactory;
+import pl.mrugames.commons.client.helpers.ClientFactoryBuilder;
 import pl.mrugames.commons.client.io.TextReader;
 import pl.mrugames.commons.client.io.TextWriter;
 import pl.mrugames.commons.host.Host;
-
-import java.util.Collections;
 
 public class Main {
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
     private static Host host;
 
-    public static void main(String ...args) throws InterruptedException {
+    public static void main(String... args) throws InterruptedException {
         if (args.length != 1) {
             logger.error("Please provide port");
             return;
@@ -23,16 +22,15 @@ public class Main {
 
         logger.info("Main started...");
 
-        ClientFactory clientFactory = new ClientFactory<>(
-                "Main Client",
-                60,
+
+        ClientFactory<String, String, String, String> clientFactory = new ClientFactoryBuilder<>(
                 TextWriter::new,
                 TextReader::new,
-                new ExampleClientWorkerFactory(Main::shutdown),
-                Collections.emptyList(),
-                Collections.emptyList(),
-                Collections.emptyList()
-        );
+                new ExampleClientWorkerFactory(Main::shutdown)
+        )
+                .setClientName("Main Client")
+                .setTimeout(60)
+                .build();
 
         host = new Host("Main Host", port, clientFactory);
 
