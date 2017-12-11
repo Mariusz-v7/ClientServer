986cdcb7d5c5b4dc67e57d41e6f41dacc1a20d93
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-12-11 09:00:52 +0100

    Prepare watchdog for new implementation + tests

--- src/main/java/pl/mrugames/client_server/client/ClientWatchdog.java
+++ src/main/java/pl/mrugames/client_server/client/ClientWatchdog.java
@@ -49,53 +49,18 @@ public class ClientWatchdog implements Runnable {
 
             logger.info("[{}] Watchdog have started in thread: {}", name, Thread.currentThread().getName());
 
-            long nextTimeout = -1;
-            int permitsToBeRestored = 0;
             while (!Thread.currentThread().isInterrupted()) {
-                // TODO: implementation is wrong at the moment...
                 try {
-                    if (nextTimeout == -1) {
-                        logger.info("[{}] There are no connections registered. Waiting.", name);
-                        semaphore.acquire();
-                    } else {
-                        logger.info("[{}] Next possible timeout in {} seconds. Waiting.", name, nextTimeout);
-                        semaphore.tryAcquire(nextTimeout, TimeUnit.SECONDS);
+                    if (semaphore.availablePermits() == 0) {
+                        logger.info("[{}] There are no connections registered.", name);
                     }
 
-                    if (permitsToBeRestored > 0) {
-                        semaphore.release(permitsToBeRestored);
-                    }
+                    semaphore.acquire();
 
-                    nextTimeout = -1;
-                    permitsToBeRestored = 0;
+                    logger.info("[{}] There are {} connections registered. Starting clean up.", name, comms.size());
+                    check();
+                    logger.info("[{}] Clean up finished. There are {} connections registered.", name, comms.size());
 
-                    logger.info("[{}] Starting cleanup.", name);
-
-                    long amount = 0;
-                    for (Container container : comms) {
-                        if (isTimeout(container.comm, container.clientName)) {
-                            logger.info("[{}] Connection is timed out, cleaning. Client: {}", name, container.clientName);
-
-                            ++amount;
-                            try {
-                                container.socket.close();
-                            } catch (Exception e) {
-                                logger.error("[{}] Error during socket close. Client: {}", name, container.clientName, e);
-                            } finally {
-                                comms.remove(container);
-                            }
-
-                            logger.info("[{}] Connection closed. Client: {}", name, container.clientName);
-                        } else {
-                            ++permitsToBeRestored;
-                            long timeout = calculateSecondsToTimeout(container.comm);
-                            if (nextTimeout == -1 || timeout < nextTimeout) {
-                                nextTimeout = timeout;
-                            }
-                        }
-
-                        logger.info("[{}] Total connections closed: {}. Amount of connections registered after cleanup: {}", name, amount, comms.size());
-                    }
                 } catch (InterruptedException e) {
                     break;
                 }
@@ -104,6 +69,10 @@ public class ClientWatchdog implements Runnable {
             running = false;
             logger.info("[{}] Watchdog have finished in thread: {}", name, Thread.currentThread().getName());
         }
+    }
+
+    void check() {
+
     }
 
     boolean isTimeout(CommV2 comm, String clientName) {
--- src/test/java/pl/mrugames/client_server/client/ClientWatchdogSpec.java
+++ src/test/java/pl/mrugames/client_server/client/ClientWatchdogSpec.java
@@ -1,23 +1,39 @@
 package pl.mrugames.client_server.client;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.Socket;
 import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ClientWatchdogSpec {
     private ClientWatchdog watchdog;
+    private ExecutorService executorService;
 
     @BeforeEach
-    void before() {
-        watchdog = new ClientWatchdog("Test", 30);
+    void before() throws InterruptedException {
+        executorService = Executors.newSingleThreadExecutor();
+        watchdog = spy(new ClientWatchdog("Test", 30));
+        executorService.execute(watchdog);
+        if (!watchdog.awaitStart(30, TimeUnit.SECONDS)) {
+            fail("Failed to start");
+        }
+    }
+
+    @AfterEach
+    void after() throws InterruptedException {
+        executorService.shutdownNow();
+        if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) {
+            fail("Failed to stop");
+        }
     }
 
     @Test
@@ -26,7 +42,12 @@ class ClientWatchdogSpec {
     }
 
     @Test
-    void whenRegister_thenIncreaseSemaphore() {
+    void whenRegister_thenIncreaseSemaphore() throws InterruptedException {
+        executorService.shutdownNow();
+        if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) { // stop it to prevent decreasing permit
+            fail("Failed to stop");
+        }
+
         watchdog.register(mock(CommV2.class), mock(Socket.class), "client");
         assertThat(watchdog.semaphore.availablePermits()).isEqualTo(1);
     }
@@ -92,5 +113,20 @@ class ClientWatchdogSpec {
         doReturn(Instant.now().minusSeconds(15)).when(comm).getLastDataReceived();
 
         assertThat(watchdog.calculateSecondsToTimeout(comm)).isEqualTo(1);
+    }
+
+    @Test
+    void givenNoConnections_whenWatchdogIsRun_thenWait() throws InterruptedException {
+        Thread.sleep(250);
+        verify(watchdog, never()).check();
+    }
+
+    @Test
+    void givenConnectionRegistered_whenWatchdogIsRun_thenCallCheck() throws InterruptedException {
+        Thread.sleep(100);
+        watchdog.register(mock(CommV2.class), mock(Socket.class), "");
+
+        Thread.sleep(100);
+        verify(watchdog, times(1)).check();
     }
 }
