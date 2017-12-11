35d72119845d5c1991674585ac6d4fd5bc9d2cf5
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-12-11 10:50:16 +0100

    Fix websocket host example

--- src/test/integration/pl/mrugames/client_server/websocket_server/Main.java
+++ src/test/integration/pl/mrugames/client_server/websocket_server/Main.java
@@ -4,9 +4,19 @@ import com.codahale.metrics.MetricRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.mrugames.client_server.HealthCheckManager;
+import pl.mrugames.client_server.client.ClientFactoryV2;
+import pl.mrugames.client_server.client.ClientWatchdog;
+import pl.mrugames.client_server.client.filters.FilterProcessorV2;
+import pl.mrugames.client_server.client.filters.StringToWebSocketFrameFilter;
+import pl.mrugames.client_server.client.filters.WebSocketFrameToStringFilter;
+import pl.mrugames.client_server.client.initializers.WebSocketInitializer;
+import pl.mrugames.client_server.client.io.WebSocketReader;
+import pl.mrugames.client_server.client.io.WebSocketWriter;
 import pl.mrugames.client_server.host.HostManager;
+import pl.mrugames.client_server.websocket.WebSocketHandshakeParser;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +25,7 @@ public class Main {
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
 
     private static HostManager hostManager;
-    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private static ExecutorService executorService = Executors.newCachedThreadPool();
 
     public static void main(String... args) throws InterruptedException, IOException {
         if (args.length != 1) {
@@ -24,31 +34,31 @@ public class Main {
         }
 
         hostManager = new HostManager();
-        executorService.execute(hostManager);
 
         final int port = Integer.valueOf(args[0]);
 
         logger.info("Main started...");
 
+        ClientWatchdog clientWatchdog = new ClientWatchdog("Test watchdog", 60);
+        executorService.execute(clientWatchdog);
+
         HealthCheckManager.setMetricRegistry(new MetricRegistry());
 
-//        ClientFactory clientFactory = ClientFactories.createClientFactoryForWSServer(
-//                "WS Server", 60, new WebSocketWorkerFactory(Main::shutdown));
+        ClientFactoryV2 clientFactory = new ClientFactoryV2<>(
+                "Test factory",
+                "Test client",
+                new WebSocketWorkerFactory(Main::shutdown),
+                Collections.singletonList(WebSocketInitializer.create(WebSocketHandshakeParser.getInstance())),
+                WebSocketWriter::new,
+                WebSocketReader::new,
+                new FilterProcessorV2(Collections.singletonList(WebSocketFrameToStringFilter.getInstance())),
+                new FilterProcessorV2(Collections.singletonList(StringToWebSocketFrameFilter.getInstance())),
+                executorService,
+                clientWatchdog
+        );
 
-//        ClientFactoryV2 clientFactory = new ClientFactoryV2<>(
-//                "Test factory",
-//                "Test client",
-//                new WebSocketWorkerFactory(Main::shutdown),
-//                Collections.singletonList(WebSocketInitializer.create(WebSocketHandshakeParser.getInstance())),
-//                WebSocketWriter::new,
-//                WebSocketReader::new,
-//                Collections.singletonList(WebSocketFrameToStringFilter.getInstance()),
-//                Collections.singletonList(StringToWebSocketFrameFilter.getInstance()),
-//                executorService,
-//                new ClientWatchdog("Test watchdog", 60)
-//        );
-//
-//        hostManager.newHost("Main Host", port, clientFactory);
+        hostManager.newHost("Main Host", port, clientFactory);
+        executorService.execute(hostManager);
 
         logger.info("Main finished...");
     }
