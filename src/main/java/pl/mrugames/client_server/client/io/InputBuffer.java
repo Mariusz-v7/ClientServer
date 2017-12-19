5e8e12817787e42edc09dec8feec83bad5d3e914
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-12-19 09:32:12 +0100

    Add reading and checking for ready implementations

--- src/main/java/pl/mrugames/client_server/client/io/InputBuffer.java
+++ src/main/java/pl/mrugames/client_server/client/io/InputBuffer.java
@@ -1,18 +1,48 @@
 package pl.mrugames.client_server.client.io;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class InputBuffer implements AutoCloseable {
     private final BufferedInputStream bufferedInputStream;
+    private final DataInputStream dataInputStream;
 
     public InputBuffer(InputStream inputStream) {
         this.bufferedInputStream = new BufferedInputStream(inputStream);
+        this.dataInputStream = new DataInputStream(bufferedInputStream);
+    }
 
+    boolean isReady() throws IOException {
+        if (bufferedInputStream.available() < 4) {
+            return false;
+        }
+
+        try {
+            bufferedInputStream.mark(4);
+            int len = dataInputStream.readInt();
+
+            return bufferedInputStream.available() >= len;
+        } finally {
+            bufferedInputStream.reset();
+        }
+    }
+
+    BufferedInputStream getBufferedInputStream() {
+        return bufferedInputStream;
+    }
+
+    /**
+     * Should be called before reading actual message. It removes length field.
+     */
+    void clearLen() throws IOException {
+        bufferedInputStream.skip(4);
     }
 
     @Override
     public void close() throws Exception {
         bufferedInputStream.close();
+        dataInputStream.close();
     }
 }
--- src/test/java/pl/mrugames/client_server/client/io/InputBufferSpec.java
+++ src/test/java/pl/mrugames/client_server/client/io/InputBufferSpec.java
@@ -2,18 +2,24 @@ package pl.mrugames.client_server.client.io;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pl.mrugames.client_server.SocketHelper;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class InputBufferSpec {
     private InputBuffer inputBuffer;
+    private DataOutputStream dataOutputStream;
 
     private SocketHelper socketHelper;
 
     @BeforeEach
     void before() throws IOException {
         socketHelper = new SocketHelper();
+        dataOutputStream = new DataOutputStream(socketHelper.getOutputStream());
 
         inputBuffer = new InputBuffer(socketHelper.getInputStream());
     }
@@ -21,6 +27,114 @@ class InputBufferSpec {
     @AfterEach
     void after() throws Exception {
         socketHelper.close();
+    }
+
+    @Test
+    void givenNothingSent_whenCheckReady_thenFalse() throws IOException {
+        assertThat(inputBuffer.isReady()).isFalse();
+    }
+
+    @Test
+    void givenLenFieldNotComplete_whenCheckReady_thenFalse() throws IOException {
+        dataOutputStream.writeByte(1);  // one byte means only 1/4 of len field
+        assertThat(inputBuffer.isReady()).isFalse();
+    }
+
+    @Test
+    void givenLenIs4_andOnly2Sent_whenCheckReady_thenFalse() throws IOException {
+        dataOutputStream.writeInt(4);
+        assertThat(inputBuffer.isReady()).isFalse();
+    }
+
+    @Test
+    void givenLenIs4_andFullMessageSent_whenCheckReady_thenTrue() throws IOException {
+        dataOutputStream.writeInt(4);
+
+        dataOutputStream.writeInt(1);
+        assertThat(inputBuffer.isReady()).isTrue();
+    }
+
+    @Test
+    void givenFullMessageArrivesInTwoSteps_whenCheckReadyTwice_firstIsFalseSecondIsTrue() throws IOException {
+        dataOutputStream.writeInt(4);
+
+        dataOutputStream.writeShort(3);
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        dataOutputStream.writeShort(3);
+        assertThat(inputBuffer.isReady()).isTrue();
+    }
+
+    @Test
+    void readTest() throws IOException {
+        dataOutputStream.writeInt(4); // len
+
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        dataOutputStream.writeInt(0);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        inputBuffer.clearLen();
+        inputBuffer.getBufferedInputStream().read(new byte[4]);
+
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        // second message
+
+        dataOutputStream.writeInt(2); // len
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        dataOutputStream.writeShort(1);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        inputBuffer.clearLen();
+        inputBuffer.getBufferedInputStream().read(new byte[2]);
+
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        // third message
+        dataOutputStream.writeShort(0);  //len 1/2
+        assertThat(inputBuffer.isReady()).isFalse();
+        dataOutputStream.writeShort(1); // len 1/2
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        dataOutputStream.writeByte(1);
+
+        assertThat(inputBuffer.isReady()).isTrue();
+    }
+
+    @Test
+    void multiplePacketsTest() throws IOException {
+        dataOutputStream.writeInt(4); // len
+        assertThat(inputBuffer.isReady()).isFalse();
+
+        dataOutputStream.writeInt(0);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        dataOutputStream.writeInt(4); // len
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        dataOutputStream.writeInt(0);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        inputBuffer.clearLen();
+        inputBuffer.getBufferedInputStream().read(new byte[4]);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        dataOutputStream.writeInt(4); // len
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        dataOutputStream.writeInt(0);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        inputBuffer.clearLen();
+        inputBuffer.getBufferedInputStream().read(new byte[4]);
+        assertThat(inputBuffer.isReady()).isTrue();
+
+        inputBuffer.clearLen();
+        inputBuffer.getBufferedInputStream().read(new byte[4]);
+        assertThat(inputBuffer.isReady()).isFalse();
+
     }
 
 }
