2e8f94338a0ca6f7f58507a0b96184b7a5e61dfb
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-01-02 10:11:04 +0100

    Add another timeout test

--- src/test/integration/pl/mrugames/client_server/client/TimeoutSpec.java
+++ src/test/integration/pl/mrugames/client_server/client/TimeoutSpec.java
@@ -13,6 +13,7 @@ import pl.mrugames.client_server.client.io.LineWriter;
 import pl.mrugames.client_server.host.HostManager;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -33,6 +34,7 @@ class TimeoutSpec {
     private TimeoutClientWorker clientWorker;
     private ExecutorService executorService;
     private LineWriter lineWriter;
+    private Comm comm;
 
     @BeforeEach
     void before() throws IOException, InterruptedException {
@@ -45,8 +47,10 @@ class TimeoutSpec {
             return lineWriter;
         },
                 LineReader::new,
-                (ClientWorkerFactory<String, String, String, String>) (comm, clientInfo, killme) ->
-                        clientWorker,
+                (ClientWorkerFactory<String, String, String, String>) (comm, clientInfo, killme) -> {
+                    this.comm = comm;
+                    return clientWorker;
+                },
                 executorService)
                 .setTimeout(timeout)
                 .setName("Text Server")
@@ -107,7 +111,42 @@ class TimeoutSpec {
         }
     }
 
-    //TODO: server keeps sending but client not = timeout
+    @Test
+    void givenServerSendsMessagesButClientNot_when5SecondsPass_thenDisconnectAnyway() throws IOException {
+        try (Socket socket = new Socket("localhost", port)) {
+            assertTimeout(ofSeconds(1), () -> clientWorker.initLatch.await());
+
+            AtomicInteger sum = new AtomicInteger();
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            executorService.submit(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        comm.send("test");
+                        while (inputStreamReader.ready()) {
+                            int c = inputStreamReader.read();
+                            logger.info("Received: {}", c);
+                        }
+                        sum.incrementAndGet();
+
+                        Thread.sleep(250);
+                    } catch (Exception e) {
+                        logger.error(e.getMessage(), e);
+                        break;
+                    }
+                }
+            });
+
+            long before = System.nanoTime();
+            assertTimeout(ofSeconds(timeout + 1), () -> clientWorker.shutdownLatch.await());
+            long after = System.nanoTime();
+
+            assertThat(TimeUnit.NANOSECONDS.toSeconds(after - before)).isGreaterThanOrEqualTo(timeout);
+
+            // 1000 / 250 = 4; 4 * 5 s = 20
+
+            assertThat(sum.get()).isCloseTo(20, Offset.offset(2));
+        }
+    }
     //TODO: both are sending = no timeout
 
     //TODO: client side timeouts
