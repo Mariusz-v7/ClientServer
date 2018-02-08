d68d9f79e36b0c5f51e9b6826644f4ca68d4067e
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-02-08 11:52:44 +0100

    Add timed out tasks removal

--- src/main/java/pl/mrugames/client_server/tasks/TaskWatchdog.java
+++ src/main/java/pl/mrugames/client_server/tasks/TaskWatchdog.java
@@ -1,22 +1,30 @@
 package pl.mrugames.client_server.tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.Instant;
 import java.util.List;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class TaskWatchdog implements Runnable {
-    final List<TaskData> tasks;
-    private final CompletionService executor;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public TaskWatchdog(CompletionService executor) {
+    final List<TaskData> tasks;
+    private final CompletionService completionService;
+    private final CountDownLatch startSignal = new CountDownLatch(1);
+    private final CountDownLatch stopSignal = new CountDownLatch(1);
+    private final Semaphore taskCount = new Semaphore(0);
+
+    public TaskWatchdog(CompletionService completionService) {
         this.tasks = new CopyOnWriteArrayList<>();
-        this.executor = executor;
+        this.completionService = completionService;
     }
 
-    void submit(Future<?> request, long timeoutSeconds) {
+    synchronized void submit(Future<?> request, long timeoutSeconds) {
         tasks.add(new TaskData(request, Instant.now(), timeoutSeconds));
+        taskCount.release();
     }
 
     long getNextPossibleTimeout() {
@@ -26,6 +34,12 @@ public class TaskWatchdog implements Runnable {
                 .mapToLong(task -> getSecondsToTimeout(task.getSubmitted(), now, task.getTimeoutSeconds()))
                 .min()
                 .orElse(0);
+    }
+
+    List<TaskData> getTimedOutList(Instant now) {
+        return tasks.stream()
+                .filter(task -> isTimeout(task.getSubmitted(), now, task.getTimeoutSeconds()))
+                .collect(Collectors.toList());
     }
 
     long getSecondsToTimeout(Instant submitted, Instant now, long timeoutSeconds) {
@@ -47,8 +61,64 @@ public class TaskWatchdog implements Runnable {
 
         //TODO: break when new task submitted
 
+        startSignal.countDown();
+
         while (!Thread.currentThread().isInterrupted()) {
+            try {
+                taskCount.acquire();
+                cycle();
+            } catch (InterruptedException e) {
+                logger.info("Watchdog interrupted");
+                break;
+            } catch (Exception e) {
+                logger.error("Error during clean up: " + e.getMessage(), e);
+            } finally {
+                taskCount.release();
+            }
+        }
+
+        stopSignal.countDown();
+    }
+
+    synchronized void cycle() throws InterruptedException {
+        long seconds = getNextPossibleTimeout();
+
+        TimeUnit.SECONDS.sleep(seconds); // TODO: use completion service and capture completed tasks
+
+        Instant now = Instant.now();
+        removeTimedOut(now);
+    }
+
+    void removeTimedOut(Instant now) {
+        List<TaskData> timedOutList = getTimedOutList(now);
+
+        for (TaskData taskData : timedOutList) {
+            tasks.remove(taskData);
+
+            logger.error("Task timed out {}", taskData);
+
+            boolean result = taskData.getResult().cancel(true);
+            if (!result) {
+                if (taskData.getResult().isDone()) {
+                    try {
+                        taskData.getResult().get();
+                    } catch (Exception e) {
+                        logger.error("Task completed with error: ", e);
+                    }
+
+                } else {
+                    logger.error("Failed to shutdown task: {}", taskData);
+                }
+            }
 
         }
+    }
+
+    void awaitStart() throws InterruptedException {
+        startSignal.await();
+    }
+
+    void awaitStop() throws InterruptedException {
+        stopSignal.await();
     }
 }
