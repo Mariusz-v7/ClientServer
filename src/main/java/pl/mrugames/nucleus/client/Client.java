1db05ce3d93c6ee842e828926191d2bea37d793d
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-04-20 14:11:22 +0200

    Implement read and write for object client

--- src/main/java/pl/mrugames/nucleus/client/Client.java
+++ src/main/java/pl/mrugames/nucleus/client/Client.java
@@ -1,13 +1,32 @@
 package pl.mrugames.nucleus.client;
 
+import pl.mrugames.nucleus.common.io.ClientReader;
+import pl.mrugames.nucleus.common.io.ClientWriter;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.net.Socket;
+import java.util.function.Function;
 
-public class Client implements AutoCloseable {
+public class Client<Write extends Serializable, Read extends Serializable> implements AutoCloseable {
     private final Socket socket;
+    private final ClientReader<Read> clientReader;
+    private final ClientWriter<Write> clientWriter;
 
-    public Client(String address, int port) throws IOException {
+    public Client(String address, int port, Function<OutputStream, ClientWriter<Write>> writerFactory, Function<InputStream, ClientReader<Read>> readerFactory) throws IOException {
         this.socket = new Socket(address, port);
+        this.clientReader = readerFactory.apply(socket.getInputStream());
+        this.clientWriter = writerFactory.apply(socket.getOutputStream());
+    }
+
+    public void write(Write frame) throws Exception {
+        clientWriter.write(frame);
+    }
+
+    public Read read() throws Exception {
+        return clientReader.read();
     }
 
     @Override
--- src/test/integration/pl/mrugames/nucleus/server/object_client/Main.java
+++ src/test/integration/pl/mrugames/nucleus/server/object_client/Main.java
@@ -2,13 +2,10 @@ package pl.mrugames.nucleus.server.object_client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.mrugames.nucleus.client.Client;
 import pl.mrugames.nucleus.common.io.ObjectReader;
 import pl.mrugames.nucleus.common.io.ObjectWriter;
 import pl.mrugames.nucleus.server.object_server.Frame;
-
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 
 public class Main {
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
@@ -24,33 +21,14 @@ public class Main {
         final String address = args[0];
         final int port = Integer.valueOf(args[1]);
 
-        Socket socket = new Socket();
-        try {
-            socket.connect(new InetSocketAddress(address, port));
+        try (Client<Frame, Frame> client = new Client<>(address, port, ObjectWriter::new, ObjectReader::new)) {
+            client.write(new Frame("Hello World!"));
+            client.write(new Frame("TEST FRAME"));
 
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
+            System.out.println(client.read());
+            System.out.println(client.read());
 
-            ObjectWriter<Frame> objectWriter = new ObjectWriter<>(outputStream);
-            objectWriter.write(new Frame("Hello World!"));
-
-            ObjectReader<Frame> objectReader = new ObjectReader<>(inputStream);
-
-            System.out.println(objectReader.read());
-
-            objectWriter.write(new Frame("shutdown"));
-
-        } finally {
-            socket.close();
+            client.write(new Frame("shutdown"));
         }
-    }
-
-    static Frame read(InputStream inputStream) throws IOException, ClassNotFoundException {
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        dataInputStream.readInt();
-
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-
-        return (Frame) objectInputStream.readUnshared();
     }
 }
